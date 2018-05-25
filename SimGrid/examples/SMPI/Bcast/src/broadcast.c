310b06ee76acac51f71fdb64d2b6b5c40c206c03
Najwa Ez Zine <nezzine@fnancy>
2018-05-25 12:03:08 +0200

    Automatic results update : Bcast

--- SimGrid/examples/SMPI/Bcast/src/broadcast.c
+++ SimGrid/examples/SMPI/Bcast/src/broadcast.c
@@ -8,24 +8,27 @@ char class;
 
 #define TRUE 1
 #define FALSE 0
-#define N 2
-#define PARSIM(min,max,D,i) if(i<=min){i=min; MPI_sleep(D[i]); } if (i>=max) {break;}
+#define PARSIM(min,max,D,i) if(i<min){i=min; printf("[%d] Sleeping\n",world_rank); ;MPI_sleep(D[0]); } if (i>=max) {break;}
+#define OUT_TEST if(world_rank==0)
 
 void MPI_sleep(int time)
 {
-  printf("Sleeping");
   int starttime = MPI_Wtime();
   while ((starttime + time) - MPI_Wtime() > 0)
   {
   };
-  return 0;
+  return ;
 }
 
 int main(int argc, char **argv)
 {
+
   int min = atoi(argv[3]);
   int max = atoi(argv[4]);
-  int D[N] = {0, 1};
+
+  int N = atoi(argv[5]);
+  int D[N];
+  D[0] = 0;
   MPI_Init(&argc, &argv);
   int buf;
   // Get the number of processes
@@ -48,13 +51,19 @@ int main(int argc, char **argv)
   char processor_name[MPI_MAX_PROCESSOR_NAME];
   int name_len;
   MPI_Get_processor_name(processor_name, &name_len);
+  if (world_rank == 0)
+  {
+    printf("Launching broadcast test\n");
+    printf("Parameters : min [%d], max[%d], N [%d]\n", min, max, N);
+    printf("MPI configuration done\n");
+  }
 
   for (int i = 0; i < N; i++)
   {
     PARSIM(min, max, D, i);
-    printf("[%d]: Before Bcast, buf is %d\n", world_rank, buf);
+    printf("[%d](i:%d): Before Bcast, buf is %d\n", world_rank, i, buf);
     MPI_Bcast(&buf, 1, MPI_INT, i % world_size, MPI_COMM_WORLD);
-    printf("[%d]: After Bcast, buf is %d\n", world_rank, buf);
+    printf("[%d](i:%d): After Bcast, buf is %d\n", world_rank, i, buf);
   }
 
   MPI_Finalize();
